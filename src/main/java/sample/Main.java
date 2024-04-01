593988c1eff44537e344bdbaf5b9f109d454e53c
大谷 斗也 <tani.touya0518@gmail.com>
2024-04-01 16:21:25 +0900

    Revert "Feature/ootani"

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -2,16 +2,9 @@ package sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Main extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
-    }
+public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
